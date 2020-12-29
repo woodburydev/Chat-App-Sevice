d11b840f6b4f7d94567fc268592144d18f8cf2f7
Nathaniel Woodbury <nate_woodbury@trendmicro.com>
2020-12-29 04:31:10 -0800

    Changes

--- src/main/java/com/codeforgeyt/wschatapplication/security/CustomWebSecurityConfigurerAdapter.java
+++ src/main/java/com/codeforgeyt/wschatapplication/security/CustomWebSecurityConfigurerAdapter.java
@@ -5,6 +5,7 @@ import com.codeforgeyt.wschatapplication.filters.UserRequestTimestamp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +15,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -44,16 +50,17 @@ public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAda
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable().authorizeRequests()
-                    .antMatchers("/user/login"  ).permitAll()
-                    .antMatchers("/user/register").permitAll()
-                    .antMatchers("/chat").permitAll()
-                    .anyRequest().authenticated()
-                    .and()
-                    .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS); // spring security dont make a session!
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/user/login"  ).permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .antMatchers("/user/register").permitAll()
+                .antMatchers("/chat").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-            // adding a filter BEFORE the username and password authentication filter
+        // adding a filter BEFORE the username and password authentication filter
             http.authenticationProvider(authProvider);
             http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
             http.cors();
@@ -65,5 +72,16 @@ public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAda
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
